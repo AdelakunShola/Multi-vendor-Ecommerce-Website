ea23b7aa7c44d5cc09553f43c01f528dba06c6c1
Adelakun Shola <officialadelakun@gmail.com>
2023-12-03 11:23:42 -1100

    create seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Guesser\Name;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+
+        DB::table('users')->insert([
+
+            //Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
+                
+            ],
+
+            //Vendor
+            [
+                'name' => 'Harry Vendor',
+                'username' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'vendor',
+                'status' => 'active',
+                
+            ],
+
+            //User / Customer
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => 'active',
+                
+            ],
+        ]);
+    }
+}
