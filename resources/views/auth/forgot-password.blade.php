b792abfe9d212566124c63d96f9aa602c5813129
Adelakun Shola <officialadelakun@gmail.com>
2023-12-11 12:22:00 -1100

    .

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -32,31 +32,52 @@
                 </div>
             </div>
         </div>
-        <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+        <div class="page-content pt-150 pb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-8 col-md-12 m-auto">
+                        <div class="row">
+                            <div class="heading_s1">
+                                <img class="border-radius-15" src="{{ asset('frontend/assets/imgs/page/reset_password.svg') }}" alt="" />
+                                <h2 class="mb-15 mt-15">Email Password Reset</h2>
+                                <p class="mb-30">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+                            </div>
+<div class="col-lg-6 col-md-8">
+    <div class="login_wrap widget-taber-content background-white">
+        <div class="padding_eight_all bg-white text-red">
 
-    <!-- Session Status -->
+           <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+<form method="POST" action="{{ route('password.email') }}">
+    @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <!-- Email Address -->
+    <div>
+        <x-input-label for="email" :value="__('Email')" />
+        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+    <div class="flex items-center justify-end mt-4">
+        <x-primary-button>
+            {{ __('Email Password Reset Link') }}
+        </x-primary-button>
+    </div>
+</form>
+        
+                                    </div>
+                                </div>
+                            </div>
+                           
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
-</x-guest-layout>
     </main>
 
      @include('frontend.body.footer')
@@ -96,3 +117,10 @@
 </body>
 
 </html>
+
+
+
+
+  
+
+    
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -22,13 +22,14 @@
     <!-- Quick view -->
    @include('frontend.body.header')
     <!--End header-->
+    <!--End header-->
 
     <main class="main pages">
         <div class="page-header breadcrumb-wrap">
             <div class="container">
                 <div class="breadcrumb">
                     <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
-                    <span></span> My Account
+                    <span></span> Pages <span></span> My Account
                 </div>
             </div>
         </div>
@@ -38,103 +39,98 @@
                     <div class="col-xl-6 col-lg-8 col-md-12 m-auto">
                         <div class="row">
                             <div class="heading_s1">
-                                <img class="border-radius-15" src="{{ asset('assets/imgs/page/reset_password.svg') }}" alt="" />
+                                <img class="border-radius-15" src="assets/imgs/page/reset_password.svg" alt="" />
                                 <h2 class="mb-15 mt-15">Set new password</h2>
                                 <p class="mb-30">Please create a new password that you don’t use on any other site.</p>
                             </div>
                             <div class="col-lg-6 col-md-8">
                                 <div class="login_wrap widget-taber-content background-white">
                                     <div class="padding_eight_all bg-white">
-
-                                    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
-
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>   
-
+                                        <form method="POST" action="{{ route('password.store') }}">
+                                            @csrf
+                                    
+                                            <!-- Password Reset Token -->
+                                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                    
+                                            <!-- Email Address -->
+                                            <div class="form-group">
+                                                <x-input-label for="email" :value="__('Email')" />
+                                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                            </div>
+                                    
+                                            <!-- Password -->
+                                            <div class="form-group col-12">
+                                                <x-input-label for="password" :value="__('Password')" />
+                                                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                            </div>
+                                    
+                                            <!-- Confirm Password -->
+                                            <div class="form-group">
+                                                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                                    
+                                                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                                                    type="password"
+                                                                    name="password_confirmation" required autocomplete="new-password" />
+                                    
+                                                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                            </div>
+                                    
+                                            <div class="flex items-center justify-end mt-4">
+                                                <x-primary-button>
+                                                    {{ __('Reset Password') }}
+                                                </x-primary-button>
+                                            </div>
+                                        </form>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-                            
+                           
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </main>
-    
-
-    
-    @include('frontend.body.footer')
+    @include('frontend.body.footer') 
+  
     <!-- Preloader Start -->
     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="text-center">
-                    <img src="{{ asset('frontend/assets/imgs/theme/loading.gif') }}" alt="" />
+                    <img src="assets/imgs/theme/loading.gif" alt="" />
                 </div>
             </div>
         </div>
     </div>
-    <!-- Vendor JS-->
-   <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/slick.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/jquery.syotimer.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/waypoints.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/wow.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/perfect-scrollbar.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/magnific-popup.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/select2.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/counterup.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/jquery.countdown.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/images-loaded.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/isotope.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/scrollup.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/jquery.vticker-min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/jquery.theia.sticky.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/plugins/jquery.elevatezoom.js') }}"></script>
-    <!-- Template  JS -->
-    <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
-    <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
-
-
-    <!-- Add this script block at the end of your HTML body -->
-
+      <!-- Vendor JS-->
+      <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/slick.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/jquery.syotimer.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/waypoints.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/wow.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/perfect-scrollbar.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/magnific-popup.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/select2.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/counterup.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/jquery.countdown.min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/images-loaded.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/isotope.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/scrollup.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/jquery.vticker-min.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/jquery.theia.sticky.js') }}"></script>
+      <script src="{{ asset('frontend/assets/js/plugins/jquery.elevatezoom.js') }}"></script>
+      <!-- Template  JS -->
+      <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
+      <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
+  
+  
 </body>
 
 </html>
