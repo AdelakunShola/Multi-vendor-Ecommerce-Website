1232ff2224ed0206e1056a32d8369e494cbb2bd9
Adelakun Shola <officialadelakun@gmail.com>
2023-12-15 11:27:27 -1100

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -98,4 +98,10 @@ class AdminController extends Controller
         return back()->with("status", "Password Updated Successfully");
 
     }//end method
+
+
+    public function InactiveVendor(){
+        $inActiveVendor = User::where('status','inactive')->where('role','vendor')->latest()->get();
+        return view('backend.vendor.inactive_vendor',compact('inActiveVendor'));
+    }//end method
 }
