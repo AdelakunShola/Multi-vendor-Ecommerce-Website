60db50d81d7ed34174734d98b0a130d2db8aa10e
Adelakun Shola <officialadelakun@gmail.com>
2023-12-17 14:06:59 -1100

    activate vendor in admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -114,4 +114,28 @@ class AdminController extends Controller
         return view('backend.vendor.active_vendor',compact('ActiveVendor'));
         
     }//end method
+
+
+    public function InactiveVendorDetails($id){
+
+        $inactiveVendorDetails = User::findOrFail($id);
+        return view('backend.vendor.inactive_vendor_details',compact('inactiveVendorDetails'));
+
+    }// End Mehtod 
+
+    public function ActiveVendorApprove(Request $request){
+
+        $verdor_id = $request->id;
+        $user = User::findOrFail($verdor_id)->update([
+            'status' => 'active',
+        ]);
+
+        $notification = array(
+            'message' => 'Vendor Activated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('active.vendor')->with($notification);
+
+    }// End Mehtod 
 }
