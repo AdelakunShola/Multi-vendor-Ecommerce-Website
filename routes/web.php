ab930878e004e4ad938234a46af16d52fb9850bf
Adelakun Shola <officialadelakun@gmail.com>
2023-12-22 04:42:08 -1100

    Backend Banner/Advert

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@ use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
+
 use App\Http\Middleware\RedirectIfAuthenticated;
 
 use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\BannerController;
 
 /* 
 |--------------------------------------------------------------------------
@@ -195,6 +197,18 @@ Route::controller(AdminController::class)->group(function(){
     Route::get('/edit/slider/{id}' , 'EditSlider')->name('edit.slider');
     Route::post('/update/slider' , 'UpdateSlider')->name('update.slider');
     Route::get('/delete/slider/{id}' , 'DeleteSlider')->name('delete.slider');
+});
+
+
+
+//Admin Banner All Route
+Route::controller(BannerController::class)->group(function(){
+    Route::get('/all/banner' , 'AllBanner')->name('all.banner');
+    Route::get('/add/banner' , 'AddBanner')->name('add.banner');
+    Route::post('/store/banner' , 'StoreBanner')->name('store.banner');
+    Route::get('/edit/banner/{id}' , 'EditBanner')->name('edit.banner');
+    Route::post('/update/banner' , 'UpdateBanner')->name('update.banner');
+    Route::get('/delete/banner/{id}' , 'DeleteBanner')->name('delete.banner');
 });
 
 
